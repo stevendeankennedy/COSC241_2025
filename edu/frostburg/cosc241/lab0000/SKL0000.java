7788395092c9bef598ce74bebec6b70893e354bb
SteveK <stevendeankennedy@users.noreply.github.com>
2025-02-04 10:21:25 -0500

    updated lab to fit test library

--- edu/frostburg/cosc241/lab0000/SKL0000.java
+++ edu/frostburg/cosc241/lab0000/SKL0000.java
@@ -1,12 +1,14 @@
 package edu.frostburg.cosc241.lab0000;
 
+import edu.frostburg.cosc241.lab.L0000.L0000JavaIntro;
+
 /**
  * Lab 0000 - Fundamental Java
  * 
  * 
  */
 
-public class SKL0000 {
+public class SKL0000 implements L0000JavaIntro {
 
 	/**
 	 * Returns a "Hello" greeting.
@@ -16,15 +18,15 @@ public class SKL0000 {
 		return "Hello";
 	}
 	
-	/**
-	 * 4) Returns x * y.  
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public int multiply(int x, int y) {
-		return x * y;
-	}
+//	/**
+//	 * 4) Returns x * y.  
+//	 * @param x
+//	 * @param y
+//	 * @return
+//	 */
+//	public int multiply(int x, int y) {
+//		return x * y;
+//	}
 	
 	/**
 	 * 5) Returns true if a number is odd, false otherwise.
@@ -39,21 +41,47 @@ public class SKL0000 {
 		}
 	}
 	
-	/**
-	 * 7) Checks if a number is prime and returns true if it is, or false
-	 * @param p a possible prime number
-	 * @return
-	 */
-	public boolean isPrime(int p) {
-		for (int i=2; i<p/2; i++) {
+	public String concat(String s1, String s2) {
+		return s1 + s2;
+	}
+
+	@Override
+	public String concat(String arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char getChar(String arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getSubstring(String arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPrime(long p) {
+		for (int i=2; i<=p/2; i++) {
 			if (p%i==0) { // if p is divisible by i, it can't be prime
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	public String concat(String s1, String s2) {
-		return s1 + s2;
+
+	@Override
+	public String removeVowels(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long multiply(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
--- edu/frostburg/cosc241/lab0000/SKL0000Driver.java
+++ edu/frostburg/cosc241/lab0000/SKL0000Driver.java
@@ -1,5 +1,7 @@
 package edu.frostburg.cosc241.lab0000;
 
+import edu.frostburg.cosc241.tests.sp24.L0000Test;
+
 /**
  * The driver class is your testing class.  This part is not graded.
  * 	Use the driver class to make sure that your code is correct.
@@ -10,17 +12,23 @@ public class SKL0000Driver {
 	public static void main(String[] args) {
 		SKL0000 lab = new SKL0000();
 		
-		String text = lab.getGreeting();
-		System.out.println(text);
-		
-		int result = lab.multiply(5, 5);
-		System.out.println(result);
-		
-		int n = 6;
-		System.out.println(lab.isOdd(n));
-		
-		int maybePrime = 100;
-		System.out.println(maybePrime + "? " + lab.isPrime(maybePrime));
+//		L0000Test myTest = new L0000Test("Your name", "the date", lab);
+		L0000Test myTest = new L0000Test("Your name", "the date", lab, "L0000Test.in", true);
+
+		myTest.run();
+//		SKL0000 lab = new SKL0000();
+//		
+//		String text = lab.getGreeting();
+//		System.out.println(text);
+//		
+//		int result = lab.multiply(5, 5);
+//		System.out.println(result);
+//		
+//		int n = 6;
+//		System.out.println(lab.isOdd(n));
+//		
+//		int maybePrime = 100;
+//		System.out.println(maybePrime + "? " + lab.isPrime(maybePrime));
 	}
 
 }
